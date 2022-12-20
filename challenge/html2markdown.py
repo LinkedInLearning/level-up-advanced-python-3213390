d99567d947019a5d8df2a87f9a87c06c462e1e46
Gregory Damiani <gregory.damiani@mobimeo.com>
2022-12-20 15:07:05 +0000

    Solution for html2markdown implementation

--- challenge/html2markdown.py
+++ challenge/html2markdown.py
@@ -1,4 +1,24 @@
+import re
+
+SPACES = re.compile(r'\s+')
+ITALICS = re.compile(r'<em>(.+?)</em>')
+PARAGRAPHS = re.compile(r'<p>(.+?)</p>')
+URLS = re.compile(r'<a href="(.+?)">(.+?)</a>')
+
 
 def html2markdown(html):
-    '''Take in html text as input and return markdown'''
-    pass
+    '''Take in a single string of html text as input and return markdown'''
+
+    # remove extraneous whitespace from html
+    markdown = SPACES.sub(r' ', html)
+
+    # next change any <em>{string}</em> to *{string}
+    markdown = ITALICS.sub(r'*\1*', markdown)
+
+    # next change any <p>{string}</p> to {string}\n\n
+    markdown = PARAGRAPHS.sub(r'\1\n\n', markdown)
+
+    # next change any <a href={URL}>TEXT</a> to [TEXT](URL)
+    markdown = URLS.sub(r'[\2](\1)', markdown)
+
+    return markdown.strip()
